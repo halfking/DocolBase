8e5c5faf1f22b21a1d975e404a87b7a24786e7f3
XUTAO HUANG <xutaohuang@XUTAOdeMacBook-Pro.local>
2021-02-04 15:36:30 +0800

    init

---
+++ DocolBase/DocolBase.h
@@ -0,0 +1,18 @@
+//
+//  DocolBase.h
+//  DocolBase
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocolBase.
+FOUNDATION_EXPORT double DocolBaseVersionNumber;
+
+//! Project version string for DocolBase.
+FOUNDATION_EXPORT const unsigned char DocolBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocolBase/PublicHeader.h>
+
+
